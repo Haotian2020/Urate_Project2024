6b9db85095f8521bab4ca7a79f4acfd5589dc645
Haotian <er20212@bristol.ac.uk>
2024-10-27 22:39:49 +0000

    add pp gwas format

--- script/04-pipeline_conti_input.R
+++ script/04-pipeline_conti_input.R
@@ -3,7 +3,7 @@
 source("fn-ukbgwas-continuous-clean.R")
 
 # read conti data --------------------------------------------------------------
-df <- data.table::fread("./data/conti_extract.csv")
+df <- data.table::fread(paste0(rdsf_personal,"data/conti_extract.csv"))
 head(df)
 colnames(df) = c("eid","sex","age","sbp1","sbp2","dbp1","dbp2","urate")
 
@@ -27,6 +27,7 @@ colnames(link) <- c("ieu","eid")
 head(link)
 
 # use continuous data cleaning function to clean each phenotype ----------------
+# full sample GWAS -------------------------------------------------------------
 
 df_urate_clean = singleconti_clean(df,"urate")
 # urate mean 308.834600400485
@@ -50,19 +51,106 @@ df_pp_clean = singleconti_clean(df,"pp_avg")
 
 # write gwas input files -------------------------------------------------------
 
-write.table(df_urate_clean,"./data/df_urate_clean.txt",
+write.table(df_urate_clean, paste0(ukb_input,"df_urate_clean.txt"),
             sep = " ",
             row.names = F, col.names = T,quote =F)
 
-write.table(df_sbp_clean,"./data/df_sbp_avg_clean.txt",
+write.table(df_sbp_clean, paste0(ukb_input,"df_sbp_avg_clean.txt"),
             sep = " ",
             row.names = F, col.names = T,quote =F)
 
-write.table(df_dbp_clean,"./data/df_dbp_avg_clean.txt",
+write.table(df_dbp_clean, paste0(ukb_input,"df_dbp_avg_clean.txt"),
             sep = " ",
             row.names = F, col.names = T,quote =F)
 
-write.table(df_pp_clean,"./data/df_pp_avg_clean.txt",
+write.table(df_pp_clean, paste0(ukb_input,"df_pp_avg_clean.txt"),
             sep = " ",
             row.names = F, col.names = T,quote =F)
 
+# Sample split GWAS ------------------------------------------------------------
+# Use the complete dataset to split -----------------------------------------------
+
+df_cpt = df[,c("eid","sex","age","sbp_avg","dbp_avg","urate")]
+# 502422
+
+df_cpt = merge(df_cpt, link, by = "eid")
+# 462826
+# remove id thats are identified as outliers in the full sample for all phenotypes
+df_cpt = subset(df_cpt, ieu %in% df_urate_clean$IID &
+                  ieu %in% df_sbp_clean$IID & 
+                  ieu %in% df_dbp_clean$IID &
+                  ieu %in% df_pp_clean$IID)
+# 413847
+sample_id = df_cpt$ieu
+# (413847+1)/2 = 206924
+set.seed(123)
+sample1_id = sample(sample_id, 206924, replace = FALSE)
+sample2_id = sample_id[sample_id%notin%sample1_id]
+
+df_urate_clean_s1 = subset(df_urate_clean, IID%in%sample1_id)
+# mean 308.7965
+# sd 79.78696
+# sample size 206924
+
+df_urate_clean_s2 = subset(df_urate_clean, IID%in%sample2_id)
+# mean 308.9642
+# sd 79.90702
+# sample size 206923
+
+df_sbp_avg_clean_s1 = subset(df_sbp_clean, FID%in%df_urate_clean_s1$FID)
+# mean 137.744
+# sd 18.31552
+# sample size 206924
+
+df_sbp_avg_clean_s2 = subset(df_sbp_clean, FID%in%df_urate_clean_s2$FID)
+# mean 137.7833
+# sd 18.3726
+# sample size 206923
+
+df_dbp_avg_clean_s1 = subset(df_dbp_clean, FID%in%df_urate_clean_s1$FID)
+# mean 82.12938
+# sd 10.04691
+# sample size 206924
+
+df_dbp_avg_clean_s2 = subset(df_dbp_clean, FID%in%df_urate_clean_s2$FID)
+# mean 82.15788
+# sd 10.08448
+# sample size 206923
+
+df_pp_avg_clean_s1 = subset(df_pp_clean, FID%in%df_urate_clean_s1$FID)
+# mean 55.6146
+# sd 13.36189
+# sample size 206924
+
+df_pp_avg_clean_s2 = subset(df_pp_clean, FID%in%df_urate_clean_s2$FID)
+# mean 55.62538
+# sd 13.40611
+# sample size 206923
+
+write.table(df_urate_clean_s1, paste0(ukb_input,"df_urate_clean_s1.txt"),
+            sep = " ",
+            row.names = F, col.names = T,quote =F)
+write.table(df_urate_clean_s2, paste0(ukb_input,"df_urate_clean_s2.txt"),
+            sep = " ",
+            row.names = F, col.names = T,quote =F)
+
+write.table(df_sbp_avg_clean_s1, paste0(ukb_input,"df_sbp_avg_clean_s1.txt"),
+            sep = " ",
+            row.names = F, col.names = T,quote =F)
+write.table(df_sbp_avg_clean_s2, paste0(ukb_input,"df_sbp_avg_clean_s2.txt"),
+            sep = " ",
+            row.names = F, col.names = T,quote =F)
+
+write.table(df_dbp_avg_clean_s1, paste0(ukb_input,"df_dbp_avg_clean_s1.txt"),
+            sep = " ",
+            row.names = F, col.names = T,quote =F)
+write.table(df_dbp_avg_clean_s2, paste0(ukb_input,"df_dbp_avg_clean_s2.txt"),
+            sep = " ",
+            row.names = F, col.names = T,quote =F)
+
+write.table(df_pp_avg_clean_s1, paste0(ukb_input,"df_pp_avg_clean_s1.txt"),
+            sep = " ",
+            row.names = F, col.names = T,quote =F)
+write.table(df_pp_avg_clean_s2, paste0(ukb_input,"df_pp_avg_clean_s2.txt"),
+            sep = " ",
+            row.names = F, col.names = T,quote =F)
--- script/05-pipeline_conti_output.R
+++ script/05-pipeline_conti_output.R
@@ -6,87 +6,116 @@ source("fn-ukbgwas-conti-output-format.R")
 # for ukb full sample gwas output format ---------------------------------------
 # urate gwas format and instruments --------------------------------------------
 
-urateclean = ContiOutputFormatFunction(dat_name = "df_urate_clean_imputed.txt.gz",outcome_name = "Urate (UKB)",
+urateclean = ContiOutputFormatFunction(dat_name = "urate_imputed.txt.gz",outcome_name = "Urate (UKB)",
                                        mean = 308.834600400485, sd = 79.895501563483, samplesize=440466)
 urate_tophits = ld_clump_local(urateclean)
 
 # write files ------------------------------------------------------------------
 
-write_tsv(urate_tophits, file = paste0(rdsf_personal, 'data/format_data/urate_clean_tophits.tsv'))
-data.table::fwrite(urateclean,paste0(rdsf_personal,"data/format_data/urate_clean_GWAS_tidy_outcome.csv"))
+write_tsv(urate_tophits, file = paste0(rdsf_personal, 'data/format_data/urate_tophits.tsv'))
+data.table::fwrite(urateclean,paste0(rdsf_personal,"data/format_data/urate_GWAS_tidy_outcome.csv"))
 
 # sbp gwas format and instruments ----------------------------------------------
 
-sbpclean = ContiOutputFormatFunction(dat_name = "df_sbp_clean_imputed.txt.gz",outcome_name = "SBP (UKB)",
-                                     mean = 139.78564403065, sd = 19.5241937805439, samplesize=432099)
+sbpclean = ContiOutputFormatFunction(dat_name = "sbp_avg_imputed.txt.gz",outcome_name = "SBP (UKB)",
+                                     mean = 137.861762634246, sd = 18.4833994911214, samplesize = 435859)
 sbp_tophits = ld_clump_local(sbpclean)
 
 # write files ------------------------------------------------------------------
 
-write_tsv(sbp_tophits, file = paste0(rdsf_personal, 'data/format_data/sbp_clean_tophits.tsv'))
-data.table::fwrite(sbpclean,paste0(rdsf_personal,"data/format_data/sbp_clean_GWAS_tidy_outcome.csv"))
+write_tsv(sbp_tophits, file = paste0(rdsf_personal, 'data/format_data/sbp_tophits.tsv'))
+data.table::fwrite(sbpclean,paste0(rdsf_personal,"data/format_data/sbp_GWAS_tidy_outcome.csv"))
 
 # dbp gwas format and instruments ----------------------------------------------
 
-dbpclean = ContiOutputFormatFunction(dat_name = "df_dbp_clean_imputed.txt.gz",outcome_name = "DBP (UKB)",
-                                     mean = 82.1392934230647, sd = 10.6322476240864, samplesize=432253 )
+dbpclean = ContiOutputFormatFunction(dat_name = "dbp_avg_imputed.txt.gz",outcome_name = "DBP (UKB)",
+                                     mean = 82.1555713017935, sd = 10.0886399141079, samplesize = 436083)
 dbp_tophits = ld_clump_local(dbpclean)
 
 # write files ------------------------------------------------------------------
-write_tsv(dbp_tophits, file = paste0(rdsf_personal, 'data/format_data/dbp_clean_tophits.tsv'))
-data.table::fwrite(dbpclean,paste0(rdsf_personal,"data/format_data/dbp_clean_GWAS_tidy_outcome.csv"))
+write_tsv(dbp_tophits, file = paste0(rdsf_personal, 'data/format_data/dbp_tophits.tsv'))
+data.table::fwrite(dbpclean,paste0(rdsf_personal,"data/format_data/dbp_GWAS_tidy_outcome.csv"))
+
+# pp gwas format and instruments -----------------------------------------------
+
+ppclean = ContiOutputFormatFunction(dat_name = "pp_avg_imputed.txt.gz",outcome_name = "PP (UKB)",
+                                     mean = 55.6526896881128, sd = 13.4170610930985, samplesize = 435478 )
+pp_tophits = ld_clump_local(ppclean)
+
+# write files ------------------------------------------------------------------
+write_tsv(pp_tophits, file = paste0(rdsf_personal, 'data/format_data/pp_tophits.tsv'))
+data.table::fwrite(ppclean, paste0(rdsf_personal,"data/format_data/pp_GWAS_tidy_outcome.csv"))
 
 # for ukb randomized sample gwas output format ---------------------------------
 # splitted samples: urate gwas format and instruments --------------------------
 
-urates1clean = ContiOutputFormatFunction(dat_name = "df_urate_clean_s1_imputed.txt.gz",outcome_name = "Urate (UKB s1)",
-                                         mean = 308.886827182596, sd = 80.0756188911649, samplesize=220082)
+urates1clean = ContiOutputFormatFunction(dat_name = "urate_s1_imputed.txt.gz",outcome_name = "Urate (UKB s1)",
+                                         mean = 308.7965, sd = 79.78696, samplesize = 206924)
 urates1_tophits = ld_clump_local(urates1clean)
 
-urates2clean = ContiOutputFormatFunction(dat_name = "df_urate_clean_s2_imputed.txt.gz",outcome_name = "Urate (UKB s2)",
-                                         mean = 308.744211449421, sd = 79.6988890362597, samplesize=220081 )
+urates2clean = ContiOutputFormatFunction(dat_name = "urate_s2_imputed.txt.gz",outcome_name = "Urate (UKB s2)",
+                                         mean = 308.9642, sd = 79.90702, samplesize = 206923 )
 urates2_tophits = ld_clump_local(urates2clean)
 
 # write files ------------------------------------------------------------------
 
-write_tsv(urates1_tophits, file = paste0(rdsf_personal, 'data/format_data/urate_clean_s1_tophits.tsv'))
-data.table::fwrite(urates1clean,paste0(rdsf_personal,"data/format_data/urate_clean_s1_GWAS_tidy_outcome.csv"))
+write_tsv(urates1_tophits, file = paste0(rdsf_personal, 'data/format_data/urate_s1_tophits.tsv'))
+data.table::fwrite(urates1clean,paste0(rdsf_personal,"data/format_data/urate_s1_GWAS_tidy_outcome.csv"))
 
-write_tsv(urates2_tophits, file = paste0(rdsf_personal, 'data/format_data/urate_clean_s2_tophits.tsv'))
-data.table::fwrite(urates2clean,paste0(rdsf_personal,"data/format_data/urate_clean_s2_GWAS_tidy_outcome.csv"))
+write_tsv(urates2_tophits, file = paste0(rdsf_personal, 'data/format_data/urate_s2_tophits.tsv'))
+data.table::fwrite(urates2clean,paste0(rdsf_personal,"data/format_data/urate_s2_GWAS_tidy_outcome.csv"))
 
 # splitted samples: sbp gwas format and instruments ----------------------------
 
-sbps1clean = ContiOutputFormatFunction(dat_name = "df_sbp_clean_s1_imputed.txt.gz",outcome_name = "SBP (UKB s1)",
-                                       mean = 139.766111866107, sd = 19.4926971630032, samplesize=205299 )
+sbps1clean = ContiOutputFormatFunction(dat_name = "sbp_avg_s1_imputed.txt.gz",outcome_name = "SBP (UKB s1)",
+                                       mean = 137.744, sd = 18.31552, samplesize = 206924 )
 sbps1_tophits = ld_clump_local(sbps1clean)
 
-sbps2clean = ContiOutputFormatFunction(dat_name = "df_sbp_clean_s2_imputed.txt.gz",outcome_name = "SBP (UKB s2)",
-                                       mean = 139.777736161105, sd = 19.5385625835847, samplesize=205580 )
+sbps2clean = ContiOutputFormatFunction(dat_name = "sbp_avg_s2_imputed.txt.gz",outcome_name = "SBP (UKB s2)",
+                                       mean = 137.7833, sd = 18.3726, samplesize = 206923 )
 sbps2_tophits = ld_clump_local(sbps2clean)
 
 # write files ------------------------------------------------------------------
 
-write_tsv(sbps1_tophits, file = paste0(rdsf_personal, 'data/format_data/sbp_clean_s1_tophits.tsv'))
-data.table::fwrite(sbps1clean,paste0(rdsf_personal,"data/format_data/sbp_clean_s1_GWAS_tidy_outcome.csv"))
+write_tsv(sbps1_tophits, file = paste0(rdsf_personal, 'data/format_data/sbp_s1_tophits.tsv'))
+data.table::fwrite(sbps1clean,paste0(rdsf_personal,"data/format_data/sbp_s1_GWAS_tidy_outcome.csv"))
 
-write_tsv(sbps2_tophits, file = paste0(rdsf_personal, 'data/format_data/sbp_clean_s2_tophits.tsv'))
-data.table::fwrite(sbps2clean,paste0(rdsf_personal,"data/format_data/sbp_clean_s2_GWAS_tidy_outcome.csv"))
+write_tsv(sbps2_tophits, file = paste0(rdsf_personal, 'data/format_data/sbp_s2_tophits.tsv'))
+data.table::fwrite(sbps2clean,paste0(rdsf_personal,"data/format_data/sbp_s2_GWAS_tidy_outcome.csv"))
 
 # splitted samples: dbp gwas format and instruments ----------------------------
 
-dbps1clean = ContiOutputFormatFunction(dat_name = "df_dbp_clean_s1_imputed.txt.gz",outcome_name = "DBP (UKB s1)",
-                                       mean = 82.1248745359047, sd = 10.6264048567287, samplesize=205238 )
+dbps1clean = ContiOutputFormatFunction(dat_name = "dbp_avg_s1_imputed.txt.gz",outcome_name = "DBP (UKB s1)",
+                                       mean = 82.12938, sd = 10.04691, samplesize = 206924 )
 dbps1_tophits = ld_clump_local(dbps1clean)
 
-dbps2clean = ContiOutputFormatFunction(dat_name = "df_dbp_clean_s2_imputed.txt.gz",outcome_name = "DBP (UKB s2)",
-                                       mean = 82.1314181167036, sd = 10.5985958366052, samplesize=205512 )
+dbps2clean = ContiOutputFormatFunction(dat_name = "dbp_avg_s2_imputed.txt.gz",outcome_name = "DBP (UKB s2)",
+                                       mean = 82.15788, sd = 10.08448, samplesize = 206923 )
 dbps2_tophits = ld_clump_local(dbps2clean)
 
 # write files ------------------------------------------------------------------
 
-write_tsv(dbps1_tophits, file = paste0(rdsf_personal, 'data/format_data/dbp_clean_s1_tophits.tsv'))
-data.table::fwrite(dbps1clean,paste0(rdsf_personal,"data/format_data/dbp_clean_s1_GWAS_tidy_outcome.csv"))
+write_tsv(dbps1_tophits, file = paste0(rdsf_personal, 'data/format_data/dbp_s1_tophits.tsv'))
+data.table::fwrite(dbps1clean,paste0(rdsf_personal,"data/format_data/dbp_s1_GWAS_tidy_outcome.csv"))
 
-write_tsv(dbps2_tophits, file = paste0(rdsf_personal, 'data/format_data/dbp_clean_s2_tophits.tsv'))
-data.table::fwrite(dbps2clean,paste0(rdsf_personal,"data/format_data/dbp_clean_s2_GWAS_tidy_outcome.csv"))
+write_tsv(dbps2_tophits, file = paste0(rdsf_personal, 'data/format_data/dbp_s2_tophits.tsv'))
+data.table::fwrite(dbps2clean,paste0(rdsf_personal,"data/format_data/dbp_s2_GWAS_tidy_outcome.csv"))
+
+
+# splitted samples: pp gwas format and instruments ----------------------------
+
+pps1clean = ContiOutputFormatFunction(dat_name = "pp_avg_s1_imputed.txt.gz",outcome_name = "PP (UKB s1)",
+                                       mean = 55.6146, sd = 13.36189, samplesize = 206924 )
+pps1_tophits = ld_clump_local(pps1clean)
+
+pps2clean = ContiOutputFormatFunction(dat_name = "pp_avg_s2_imputed.txt.gz",outcome_name = "PP (UKB s2)",
+                                       mean = 55.62538, sd = 13.40611, samplesize = 206923 )
+pps2_tophits = ld_clump_local(pps2clean)
+
+# write files ------------------------------------------------------------------
+
+write_tsv(pps1_tophits, file = paste0(rdsf_personal, 'data/format_data/pp_s1_tophits.tsv'))
+data.table::fwrite(pps1clean,paste0(rdsf_personal,"data/format_data/pp_s1_GWAS_tidy_outcome.csv"))
+
+write_tsv(pps2_tophits, file = paste0(rdsf_personal, 'data/format_data/pp_s2_tophits.tsv'))
+data.table::fwrite(pps2clean,paste0(rdsf_personal,"data/format_data/pp_s2_GWAS_tidy_outcome.csv"))
