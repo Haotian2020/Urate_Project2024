c7a72cd5795ad1f87bed2906f9e8b3a349337c73
Haotian <er20212@bristol.ac.uk>
2024-11-14 23:59:25 +0000

    add r2 number

--- script/fn-ld_clump_local.R
+++ script/fn-ld_clump_local.R
@@ -1,29 +1,44 @@
 # function for doing LD clump in local
 
-ld_clump_local =  function(out_dat, threshold = 5e-8) {
-  
-  if("pval.outcome" %in% colnames(out_dat)){
+ld_clump_local =  function(out_dat,
+                           threshold = 5e-8,
+                           r2 = 0.001,
+                           ignore_samplesize = F) {
+  if ("pval.outcome" %in% base::colnames(out_dat)) {
     tmp = subset(out_dat, pval.outcome < threshold) %>% TwoSampleMR::convert_outcome_to_exposure()
-    }else
-    {
+  } else
+  {
     tmp = subset(out_dat, pval.exposure < threshold)
-    }
+  }
   
   snps = ieugwasr::ld_clump(
     dplyr::tibble(rsid = tmp$SNP,
                   pval = tmp$pval.exposure),
     clump_kb = 10000,
-    clump_r2 = 0.001,
+    clump_r2 = r2,
     clump_p = 0.99,
     plink_bin = genetics.binaRies::get_plink_binary(),
     bfile = paste0(rdsf_personal, "data/1kg_eur/EUR")
   )
   
-  ins = subset(tmp,SNP%in%snps$rsid)
-  ss = subset(out_dat,SNP%in%snps$rsid) %>% dplyr::select(c("SNP",starts_with("chr"), starts_with("pos"), starts_with("samplesize")))
-  colnames(ss) = c("SNP","chr.exposure","pos.exposure","samplesize.exposure")
+  ins = subset(tmp, SNP %in% snps$rsid)
+  if (ignore_samplesize == F) {
+    ss = subset(out_dat, SNP %in% snps$rsid) %>%  dplyr::select(c(
+      "SNP",
+      starts_with("chr"),
+      starts_with("pos"),
+      starts_with("samplesize")
+    ))
+    base::colnames(ss) = c("SNP",
+                           "chr.exposure",
+                           "pos.exposure",
+                           "samplesize.exposure")
+  } else {
+    ss = subset(out_dat, SNP %in% snps$rsid) %>%  dplyr::select(c("SNP", starts_with("chr"), starts_with("pos")))
+    base::colnames(ss) = c("SNP", "chr.exposure", "pos.exposure")
+  }
   
-  ins = merge(ins,ss,by ="SNP")
+  ins = merge(ins, ss, by = "SNP")
   
   return(ins)
 }
