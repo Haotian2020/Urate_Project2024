e9d3195ea433d4f4e2129d989dd5e13c4ccb769e
Haotian <er20212@bristol.ac.uk>
2025-01-09 14:49:41 +0000

    modify function for plots with Steiger Filtering

--- script/fn-uvmr_scatter.R
+++ script/fn-uvmr_scatter.R
@@ -1,20 +1,29 @@
 # function for plotting scatter plot for UVMR results
 
-save_scatter_plot = function(dat,res){
+save_scatter_plot = function(dat, res, sf = 0){
   
   p1 <- mr_scatter_plot(res, dat)
-  ggsave(p1[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"_scatterplot.pdf"))
   
   res_single <- mr_singlesnp(dat)
   p2 <- mr_forest_plot(res_single)
-  ggsave(p2[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"_forestplot.pdf"))
   
   res_loo <- mr_leaveoneout(dat)
   p3 <- mr_leaveoneout_plot(res_loo)
-  ggsave(p3[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"_leaveoneout.pdf"))
   
   res_single <- mr_singlesnp(dat)
   p4 <- mr_funnel_plot(res_single)
-  ggsave(p4[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"_funnelplot.pdf"))
-
+  
+  if(sf == 0){
+  ggsave(p1[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"_scatterplot.pdf"))
+  ggsave(p2[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"_forestplot.pdf"))
+  ggsave(p3[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"_leaveoneout.pdf"))
+  ggsave(p4[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"_funnelplot.pdf"))}
+  
+  if(sf == 1){
+    ggsave(p1[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"sf_scatterplot.pdf"))
+    ggsave(p2[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"sf_forestplot.pdf"))
+    ggsave(p3[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"sf_leaveoneout.pdf"))
+    ggsave(p4[[1]], file = paste0(rdsf_personal,"results/",unique(res$exposure),"_on_",unique(res$outcome),"sf_funnelplot.pdf"))
+  }
+  
 }
