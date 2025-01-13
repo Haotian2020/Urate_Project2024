63a5989fae3da9e1fd4ffdf9533c1aaee653e576
Haotian <er20212@bristol.ac.uk>
2025-01-13 14:21:18 +0000

    Update 10-plot_bidirection_uvmr.R

--- script/10-plot_bidirection_uvmr.R
+++ script/10-plot_bidirection_uvmr.R
@@ -9,8 +9,8 @@ res = fread(paste0(rdsf_personal,"results/results_bin.csv"))
 res_sf = fread(paste0(rdsf_personal,"results/results_sf_bin.csv"))
 
 # draw -------------------------------------------------------------------------
-# Options: 
-# "Urate (CKDGen)" "eGFR (CKDGen2019)"  "Urate (UKB)"  "SBP (UKB)"    "DBP (UKB)"
+# Options for exposure: 
+# "Urate (CKDGen)" "eGFR (CKDGen2019)"  "Urate (UKB)"  "SBP (UKB)"   "DBP (UKB)" "PP (UKB)"
 
 dev.off()
 p = uvmr_plot(dat = res,
@@ -59,22 +59,6 @@ mtext("C)",side = 3,line = 2,adj = 0, cex = 1.5,padj = 0)
 print(p)
 dev.off()
 
-p = uvmr_plot(dat = res,
-              exp = "eGFR (CKDGen2019)",
-              out = c("SBP (UKB)", "DBP (UKB)", ,"Urate (CKDGen)"),
-              line_number = 2,
-              xlabel = "Beta (with 95% CI) for each continuous outcome per SD unit change in log(eGFR)",
-              x_ticks = c(-0.25,-0.2,-0.15,-0.1,-0.05,0,0.05,0.1),
-              intervals = c(-0.25,0.1))
-
-p
-
-pdf(paste0(rdsf_personal,"results/egfr on each outcome forestplot.pdf"),width = 18, height = 5)
-plot.new()
-mtext("D)",side = 3,line = 2,adj = 0, cex = 1.5,padj = 0)
-print(p)
-dev.off()
-
 # add pp plot
 p = uvmr_plot(dat = res,
               exp = "PP (UKB)",
@@ -88,9 +72,22 @@ p
 
 pdf(paste0(rdsf_personal,"results/pp on each outcome forestplot.pdf"),width = 18, height = 3.5)
 plot.new()
-mtext("E)",side = 3,line = 2,adj = 0, cex = 1.5,padj = 0)
+mtext("D)",side = 3,line = 2,adj = 0, cex = 1.5,padj = 0)
 print(p)
 dev.off()
 
+p = uvmr_plot(dat = res,
+              exp = "eGFR (CKDGen2019)",
+              out = c("SBP (UKB)", "DBP (UKB)", "PP (UKB)", "Urate (CKDGen)"),
+              line_number = 2,
+              xlabel = "Beta (with 95% CI) for each continuous outcome per SD unit change in log(eGFR)",
+              x_ticks = c(-0.25,-0.2,-0.15,-0.1,-0.05,0,0.05,0.1),
+              intervals = c(-0.25,0.1))
 
+p
 
+pdf(paste0(rdsf_personal,"results/egfr on each outcome forestplot.pdf"),width = 18, height = 5)
+plot.new()
+mtext("E)",side = 3,line = 2,adj = 0, cex = 1.5,padj = 0)
+print(p)
+dev.off()
