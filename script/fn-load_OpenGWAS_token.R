ab490fe1e30b2ad240af4ff4179fad39524fef53
Haotian <er20212@bristol.ac.uk>
2025-01-14 09:45:39 +0000

    Create fn-load_OpenGWAS_token.R

---
+++ script/fn-load_OpenGWAS_token.R
@@ -0,0 +1,4 @@
+# add your token to .Renviron file if you have launched the R session from the terminal
+# desciption on website: https://mrcieu.github.io/ieugwasr/articles/guide.html#authentication
+
+readRenviron("~/.Renviron")
