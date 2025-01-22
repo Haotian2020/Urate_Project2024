45f000c118ea0f3e2bb19ae24ba171ccd373e2ca
Haotian <er20212@bristol.ac.uk>
2025-01-22 10:25:39 +0000

    add pp into figures

--- script/10-plot_bidirection_uvmr.R
+++ script/10-plot_bidirection_uvmr.R
@@ -3,7 +3,6 @@
 source(fn-uvmrplot.R)
 
 # reading results and format data ----------------------------------------------
-
 res = fread(paste0(rdsf_personal,"results/results_bin.csv"))
 
 res_sf = fread(paste0(rdsf_personal,"results/results_sf_bin.csv"))
@@ -16,12 +15,12 @@ dev.off()
 p = uvmr_plot(dat = res,
               exp = "Urate (CKDGen)",
               out = c("eGFR (CKDGen2019)", "SBP (UKB)", "DBP (UKB)", "PP (UKB)"),
-              line_number = 2,
+              line_number = 3,
               xlabel = "Beta (with 95% CI) for each continuous outcome per SD unit change in urate",
               x_ticks = c(-0.2,-0.1,0,0.1,0.2),
               intervals = c(-0.2,0.2))
 
-pdf(paste0(rdsf_personal,"results/exurate on each outcome forestplot.pdf"),width = 18, height = 5)
+pdf(paste0(rdsf_personal,"results/exurate on each outcome forestplot.pdf"),width = 18, height = 6)
 plot.new()
 mtext("A)",side = 3,line = 2,adj = 0, cex = 1.5,padj = 0)
 print(p)
@@ -79,14 +78,14 @@ dev.off()
 p = uvmr_plot(dat = res,
               exp = "eGFR (CKDGen2019)",
               out = c("SBP (UKB)", "DBP (UKB)", "PP (UKB)", "Urate (CKDGen)"),
-              line_number = 2,
+              line_number = 3,
               xlabel = "Beta (with 95% CI) for each continuous outcome per SD unit change in log(eGFR)",
               x_ticks = c(-0.2,-0.1,0,0.1,0.2),
               intervals = c(-0.2,0.1))
 
 p
 
-pdf(paste0(rdsf_personal,"results/egfr on each outcome forestplot.pdf"),width = 18, height = 5)
+pdf(paste0(rdsf_personal,"results/egfr on each outcome forestplot.pdf"),width = 18, height = 6)
 plot.new()
 mtext("E)",side = 3,line = 2,adj = 0, cex = 1.5,padj = 0)
 print(p)
